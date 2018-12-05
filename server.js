fd45690c61cff3d36f99b82eecd879ed191e1581
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-12-04 22:31:09 -0800

    first draft

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const server = require('http').createServer(app);
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
+
 
 server.listen(process.env.PORT || port, function() {
 	console.log(`Node server started ${port}`)
